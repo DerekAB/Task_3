205c42507436e9b8c86b0131848c9166388fd56e
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-06 23:46:33 +0000

    finished

--- Task#3.py
+++ Task#3.py
@@ -1,5 +1,15 @@
+#Name:                  Task 2.py
+#Author:                Derek Baker
+#Date Created:          06-02-2023
+#Date Last Modified:    06-02-2023
+#
+#Purpose:
+#
+#The purpose of this program is to ask the user for a username. The program will require the user to have at least 1 capital letter and 1 number included in the username.
+#The program will loop until they enter a valid username.
+
 def greeting(userName):
-    if len(userName) < 2:
+    if len(userName) < 2:                                           #function that will check the username specifics and loop until a valid username is entered
         print("Username cannot be less than 2 characters.")
         return True
     if len(userName) > 20:
@@ -10,10 +20,13 @@ def greeting(userName):
         return True
     for char in userName:
         if char.isnumeric():
-            print("Username requires at least 1 number.")
-            return True
-    
-userNameInput = input("Please enter a username. Username should have at least 1 capital letter and 1 number: ")
+            return False
+    print("Username requires at least 1 number.")
+    return True
+
+userNameInput = input("Please enter a username. Username should have at least 1 capital letter and 1 number: ")     #The first input the user will see
 
 while greeting(userNameInput):
-    userNameInput = input("Please enter a username. Username should have at least 1 capital letter and 1 number: ")
+    userNameInput = input("Please enter a username. Username should have at least 1 capital letter and 1 number: ")     #when the user enters a name, it will go through the function to be checked
+    
+print("Your username is " + userNameInput.strip())      #print the user's username to them and end the program
